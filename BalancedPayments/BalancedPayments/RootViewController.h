a7d53bef4fda6489a666399cf554ec14f314a775
Victor Lin <bornstub@gmail.com>
2014-12-29 14:56:42 -0800

    Implement the UI structure

---
+++ BalancedPayments/BalancedPayments/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  BalancedPayments
+//
+//  Created by Victor Lin on 2014/12/29.
+//  Copyright (c) 2014年 Balanced Payments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+
+@end
---
+++ BalancedPayments/BalancedPayments/RootViewController.m
@@ -0,0 +1,41 @@
+//
+//  RootViewController.m
+//  BalancedPayments
+//
+//  Created by Victor Lin on 2014/12/29.
+//  Copyright (c) 2014年 Balanced Payments. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    _sidebarButton.target = self.revealViewController;
+    _sidebarButton.action = @selector(revealToggle:);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
